2d7b76d9fe6678c0a9f9a9bae7b1f9940ba9ffc6
Oscar Cisneros <cisneros_olcj@outlook.com>
2014-08-26 20:05:16 -0500

    fixed migration

--- db/migrate/20140730020257_change_service_calltablecolumn_statusto_boolean.rb
+++ db/migrate/20140730020257_change_service_calltablecolumn_statusto_boolean.rb
@@ -1,5 +1,9 @@
 class ChangeServiceCalltablecolumnStatustoBoolean < ActiveRecord::Migration
   def change
-     change_column :service_calls, :status, :boolean, default: false
+     # change_column :service_calls, :status, :boolean, default: false
+     change_table :service_calls do |t|
+        t.remove :status
+        t.boolean :status, default: false
+     end
   end
 end
